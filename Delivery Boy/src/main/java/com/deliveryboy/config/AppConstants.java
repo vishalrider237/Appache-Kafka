011e257daf99aef94c6e365e0a2eab3408afe0ef
Vishal Pandey <vishalpandey10022000@gmail.com>
2023-08-30 18:40:34 +0530

    Add files via upload

---
+++ Delivery Boy/src/main/java/com/deliveryboy/config/AppConstants.java
@@ -0,0 +1,5 @@
+package com.deliveryboy.config;
+
+public class AppConstants {
+    public static final String LOCATION_UPDATE_TOPIC="location-update-topic";
+}
---
+++ Delivery Boy/src/main/java/com/deliveryboy/config/KafkaConfig.java
@@ -0,0 +1,15 @@
+package com.deliveryboy.config;
+
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.TopicBuilder;
+
+@Configuration
+public class KafkaConfig {
+
+    @Bean
+    public NewTopic topic(){
+        return TopicBuilder.name(AppConstants.LOCATION_UPDATE_TOPIC).build();
+    }
+}
